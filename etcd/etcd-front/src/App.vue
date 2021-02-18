4aeb8ea0e3b88c1b24cce2a91279e76da25d4727
刘乾洪 <liuqianhong@xmfunny.com>
2021-02-18 19:15:25 +0800

    <feature> 添加 etcd-front

---
+++ etcd/etcd-front/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld :msg="indexMsg" @click="onChangeIndexMsg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    onChangeIndexMsg: () =>{
+      this.store.commit('changeIndexMsg',"changed text");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ etcd/etcd-front/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue"
+import Vuex from "vuex"
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state:{
+        indexMsg: "Welcome to Your Vue.js App"
+    },
+    mutations:{
+        changeIndexMsg(state,updateMsg){
+            state.indexMsg = updateMsg;
+        }
+    }
+})
+
+export default{
+    store
+}
